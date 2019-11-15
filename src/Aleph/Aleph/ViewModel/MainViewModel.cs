a67dfc3248fa3f4fb67b8e1aec61e94e31ed626c
Matt McManis <mattmcmanis@outlook.com>
2019-11-15 06:54:11 -0800

    Update MainViewModel.cs

--- src/Aleph/Aleph/ViewModel/MainViewModel.cs
+++ src/Aleph/Aleph/ViewModel/MainViewModel.cs
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License
 along with this program.If not, see <http://www.gnu.org/licenses/>. 
 ---------------------------------------------------------------------- */
 
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows;
 // Disable XML Comment warnings
